fb63bab20ca2fc75219fd3b85562dc1cd0a3f708
killuazoldyck680 <killuaz2049@gmail.com>
2026-06-04 22:36:04 +0530

    feat: initialize currency converter repository and implement basic clap argument structures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser,Subcommand};
+
+
+
+
+#[derive(Parser)]
+#[command(name = "curr", version = "1.0", about = "Universal Currency Converter")]
+struct CLI {
+    #[command(subcommand)]
+    subcommand: Commands,
+}
+
+#[derive(Subcommand,Debug)]
+enum Commands {
+    Convert {
+        amount: f64,
+        from: String,
+        to: String,
+    }
 }
